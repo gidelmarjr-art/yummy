a6307fb64fac625f4731553b8b9b766e10a4b997
thalinetts <thalinethaisteles@gmail.com>
2026-08-31 22:45:22 -0300

    feat(carrinho): implementa tela de carrinho, componente CartItem e estrutura das próximas páginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './apps/web/pages/Landing/Landing';
 import CadastroCliente from './apps/web/pages/Cliente/CadastroCliente/CadastroCliente';
 import LoginCliente from './apps/web/pages/Cliente/LoginCliente/LoginCliente';
 import Home from './apps/web/pages/Cliente/Homepage/Homepage';
+import Cart from './apps/web/pages/Cliente/Carrinho/Carrinho';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/cadastro" element={<CadastroCliente />} />
         <Route path="/login" element={<LoginCliente />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/carrinho" element={<Cart />} />
       
       </Routes>
     </BrowserRouter>
