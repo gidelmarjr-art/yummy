902497f5438a618ac282512b986313ae37fccaf1
gidelmarjr-art <gidelmarjr@gmail.com>
2026-09-08 05:13:05 -0300

    Atualizações do Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 import app.models.usuario
+from app.routers import auth_router
 
 # Cria as tabelas no banco de dados se não existirem
 Base.metadata.create_all(bind=engine)
@@ -20,37 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Importação do router e inclusão na aplicação
-from app.routers import auth_router
-app.include_router(auth_router.router)
-
-@app.get("/")
-def main():
-    return {"mensagem": "Servidor rodando com sucesso!"}
-
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.database import engine, Base
-import app.models.usuario
-
-# Apaga as tabelas antigas e recria com as colunas novas
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(
-    title="API Delivery",
-    version="1.0.0"
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-from app.routers import auth_router
+# Inclusão do router
 app.include_router(auth_router.router)
 
 @app.get("/")
